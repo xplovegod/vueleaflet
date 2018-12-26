6206ed602a327fb77105e156b42ec929d2602d52
xplovegod <zxp_3000@126.com>
2018-12-26 22:23:07 +0800

    initial commit

---
+++ src/components/VueLeaflet.vue
@@ -0,0 +1,40 @@
+<template>
+	  <div class="vue-leaflet">
+	    <l-map class="mymap" :zoom="zoom" :center="center">
+	      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+	      <l-marker :lat-lng="marker">
+	        <l-popup :content="text"></l-popup>
+	      </l-marker>
+	    </l-map> 
+	  </div>
+</template>
+
+<script>
+	import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet';
+
+	export default{
+		name: 'VueLeaflet',
+		components:{
+			LMap,
+			LTileLayer,
+			LMarker,
+			LPopup
+		},
+		data(){
+			return {
+		      zoom: 13,
+		      center: L.latLng(47.413220, -1.219482),
+		      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+		      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+		      marker: L.latLng(47.413220, -1.219482),
+		      text: 'this is a marker'
+		    }
+		}
+	}
+</script>
+
+<style scoped>
+	.mymap{
+		width: 400px; height: 400px;
+	}
+</style>
